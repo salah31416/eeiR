fc15995228c8b9aa0d28ffefdb881cc2d5f7b3fd
Salah Adine <salah3.1416@gmail.com>
2019-05-04 14:50:23 -0300

    update esg

--- R/esg.R
+++ R/esg.R
@@ -4,7 +4,7 @@
 esg = function(a = 0, b = 0, c = 0, 
 			   k1 = 1, k2 = 0.8, k3 = 0.6, 
 			   w1 = 0.8, w2 = 1, w3 = 1, 
-			   type = c("1", "2"))
+			   type = c("esg1", "esg2"))
 {
 	if(is.null(a)) a = 0
 	if(is.null(b)) b = 0
@@ -12,8 +12,8 @@ esg = function(a = 0, b = 0, c = 0,
 
 	type <- match.arg(type)
 	switch(type,
-		   esgi = {ei = a*k1 + b*k2 + c*k3},
-		   esgii = {ei = a*w1 + b*w2 + c*w3})
+		   esg1 = {ei = a*k1 + b*k2 + c*k3},
+		   esg2 = {ei = a*w1 + b*w2 + c*w3})
 
 	return(ei)
 }#end esg
