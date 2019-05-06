09a33eddfdcd341a5d7ae9484d39958fa026a3e4
Salah Adine <salah3.1416@gmail.com>
2019-05-06 16:08:49 -0300

    updated

---
+++ R/esc.R
@@ -0,0 +1,26 @@
+##-------------------------------------------------------------
+## Ecological Status Classes
+##
+## 0-0.2 = bad
+## 0.2-0.4 = poor
+## 0.4-0.6 = moderate
+## 0.6-0.8 = good
+## 0.8-1.0 = high
+##-------------------------------------------------------------
+esc = function(x)
+{
+	.esc = function(ic)
+	{
+		if(ic <= .2) cl = "bad"
+		if(ic > .2 & ic <= .4) cl = "poor"
+		if(ic > .4 & ic <= .6) cl = "moderate"
+		if(ic > .6 & ic <= .8) cl = "good"
+		if(ic > .8) cl = "high"
+
+		return(cl)
+	}#end .esc
+
+	cl = sapply(x, .esc)
+
+	return(cl)
+}#end esc
