a9da84bfcdd5a15df897d3240c98a2dbadabc99e
Salah Adine <salah3.1416@gmail.com>
2019-05-07 20:03:56 -0300

    updated

--- R/esg.R
+++ R/esg.R
@@ -1,19 +1,18 @@
 ##-------------------------------------------------------------
 ## Ecological Status Group
 ##-------------------------------------------------------------
-esg = function(a = 0, b = 0, c = 0, 
-			   k1 = 1, k2 = 0.8, k3 = 0.6, 
-			   w1 = 0.8, w2 = 1, w3 = 1, 
-			   type = c("esg1", "esg2"))
+esg = function(x = 0, y = 0, z = 0, k = NULL, type = c("esg1", "esg2"))
 {
-	if(is.null(a)) a = 0
-	if(is.null(b)) b = 0
-	if(is.null(c)) c = 0
+	type = match.arg(type)
 
-	type <- match.arg(type)
-	switch(type,
-		   esg1 = {ei = a*k1 + b*k2 + c*k3},
-		   esg2 = {ei = a*w1 + b*w2 + c*w3})
+	if(is.null(x)) x = 0
+	if(is.null(y)) y = 0
+	if(is.null(z)) z = 0
+
+	if(is.null(k) & type == "esg1") k = c(1, 0.8, 0.6) else k = k
+	if(is.null(k) & type == "esg2") k = c(0.8, 1, 1) else k = k
+
+	ei = x*k[1] + y*k[2] + z*k[3]
 
 	return(ei)
 }#end esg
