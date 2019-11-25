deb6138774810e5feaeca807736b253e7afd2331
Salah Adine <salah3.1416@gmail.com>
2019-11-25 19:38:05 -0300

    atualiza

--- R/desg.R
+++ R/desg.R
@@ -19,13 +19,13 @@ desg = function(dt,
 
 	cove = parse(text=cove)
 
-	ia = substitute(ia)
-	ib = substitute(ib)
-	ic = substitute(ic)
+	ia = parse(text=ia)
+	ib = parse(text=ib)
+	ic = parse(text=ic)
 
-	iia = substitute(iia)
-	iib = substitute(iib)
-	iic = substitute(iic)
+	iia = parse(text=iia)
+	iib = parse(text=iib)
+	iic = parse(text=iic)
 
 	D1 = EE[, list(Total = sum(eval(cove))), 
 			by = c(group, site, replica)]
@@ -33,7 +33,7 @@ desg = function(dt,
 	func = paste(paste(c(site, replica), collapse = " + "), 
 				"~", paste(group, collapse = " + "))
 	
-	D2 = dcast(D1, func, value.var = "Total")
+	D2 = dcast(D1, func, value.var = "Total", fill = 0)
 
 	D3 = D2[, list(ESGI = esg(eval(ia), eval(ib), eval(ic), type = "esg1"),
 				   ESGII = esg(eval(iia), eval(iib), eval(iic), type = "esg2")),
@@ -46,6 +46,12 @@ desg = function(dt,
 	D5 = D4[, list(EEI = mean(EEI), EQR = mean(EQR)), by = site][,
 					list(ESC = esc(EEI)), by = c(site, "EEI", "EQR")]
 
-	return(list("coverage" = D2, "esg" = D3, "eei" = D4, "average" = D5))
+	#return(list("coverage" = D2, "esg" = D3, "eei" = D4, "average" = D5))
+
+	nm = names(D2)[1]
+	OUT1 = D2[D3, on=nm]
+	OUT = OUT1[D4, on=nm]
+
+	return(list("EEI"=OUT, "average"=D5))
 
 }#end desg
